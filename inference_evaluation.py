aec143c16619021be20852e300c213a667da619d
rratshin-wandb <russell.ratshin@wandb.com>
2024-09-12 18:47:17 -0700

    Create inference_evaluation.py

---
+++ inference_evaluation.py
@@ -0,0 +1,21 @@
+import os, sys, time
+import wandb
+wandb.require("core")
+
+PROJECT_NAME="eval-llama-3.1-8b-fine-tune"
+ENTITY="reviewco"
+
+def main():
+
+    time.sleep(180)
+    print("[" + get_tstamp() + "] Evaluation inference deployment successful.")
+
+def get_tstamp():
+    # get timestamp
+    from datetime import datetime
+    tstamp = datetime.now().strftime("%Y%m%d%H%M%S%f") # 20240910113645272981
+    # tstamp = tstamp[:16]
+    return tstamp
+
+if __name__ == "__main__":
+    main()
